5718651a4d17993aeac5c39341274210e3a1204b
Tess <srl447@nyu.edu>
2021-01-06 13:34:09 -0500

    Intro do dialogue system

---
+++ Laplace/Assets/Scripts/VN/IntroText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroText : MonoBehaviour
+{
+    TextControl textC;
+    int count = 0;
+    private string[] t = new string[]
+    {
+        "Ugh why is it early:Modayaal",
+        "Time to grab a drink",
+        "Where did I put it",
+        "Oh Yeah!   ",
+        "I can make it::A"
+
+    };
+
+    public string[] T { get => t; set => t = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textC = TextControl.instance;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(!textC.isSpeaking || textC.waitForInput)
+            {
+                if(count >= T.Length)
+                {
+                    return;
+                }
+
+                Say(T[count]);
+                count++;
+            }
+        }
+        
+    }
+    void Say(string s)
+    {
+        string[] part = s.Split(':');
+        string text = part[0];
+        string speaker = (part.Length >= 2) ? part[1] : "";
+        bool a = (part.Length >= 3) ? true : false;
+
+        textC.Say(text,a,speaker);
+    }
+}
---
+++ Laplace/Assets/Scripts/VN/TextControl.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextControl : MonoBehaviour
+{
+    public static TextControl instance;
+    public Elements elements;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void Say(string speech, bool additive = false, string speaker = "")
+    {
+        StopSpeaking();
+        mainText.text = targetText;
+        speaking = StartCoroutine(Speaking(speech, additive, speaker));
+
+    }
+
+    void StopSpeaking()
+    {
+        if (isSpeaking)
+        {
+            StopCoroutine(speaking);
+        }
+        speaking = null;
+    }
+
+    public bool isSpeaking {get{return speaking != null;}}
+    public bool waitForInput = false;
+
+    string targetText = "";
+    Coroutine speaking = null;
+    IEnumerator Speaking(string inText, bool additive, string speaker = "")
+    {
+        textBox.SetActive(true);
+        targetText = inText;
+        if (!additive)
+            mainText.text = "";
+        else
+            targetText = mainText.text + targetText;
+        speakerName.text = DetermineSpeaker(speaker);
+        waitForInput = false;
+
+        while(mainText.text != targetText)
+        {
+            mainText.text += targetText[mainText.text.Length];
+            yield return new WaitForEndOfFrame();
+        }
+
+        waitForInput = true;
+        while(waitForInput)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        StopSpeaking();
+    }
+
+    string DetermineSpeaker(string s)
+    {
+        string speaker = speakerName.text;
+        if(s!= "" && s != speakerName.text)
+        {
+            speaker = (s.ToLower().Contains("narrator")) ? "" : s;
+        }
+        return speaker;
+    }
+    [System.Serializable] //allows for recreation of class when needed
+    public class Elements
+    {
+        /*
+         * Contains all the elements of the visual novel
+         */
+
+        public GameObject textBox;
+        public Text speakerName;
+        public Text mainText;
+    }
+
+    //So there's no need to reference elements every time
+    public GameObject textBox { get { return elements.textBox; } }
+    public Text speakerName { get { return elements.speakerName; } }
+    public Text mainText { get { return elements.mainText; } }
+}
+
