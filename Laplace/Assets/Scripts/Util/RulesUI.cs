754b895ce190a05fba8a3f3eab2d1d9e0a60fd42
Tess <srl447@nyu.edu>
2021-03-15 18:15:40 -0400

    Rules working

---
+++ Laplace/Assets/Scripts/Util/RulesUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RulesUI : MonoBehaviour
+{
+    public Sprite[] winImages;
+    public GameObject left, right;
+    int index = 0;
+
+    public void LeftButton()
+    {
+        if(index > 0)
+        {
+            if(index >= 3)
+            {
+                right.SetActive(true);
+            }
+            index--;
+            GetComponent<Image>().sprite = winImages[index];
+            if(index <= 0)
+            {
+                left.SetActive(false);
+            }
+        }
+    }
+
+    public void RightButton()
+    {
+        if (index < 3)
+        {
+            if (index <= 0)
+            {
+                left.SetActive(true);
+            }
+            index++;
+            GetComponent<Image>().sprite = winImages[index];
+            if (index >= 3)
+            {
+                right.SetActive(false);
+            }
+        }
+    }
+
+    public void CanClick()
+    {
+        GameManager.Instance.canClick = true;
+    }
+
+    public void CantClick()
+    {
+        GameManager.Instance.canClick = false;
+    }
+}
