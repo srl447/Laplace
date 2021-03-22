c9b33411bc204ebec8e0817f34b69a4243196752
Tess <srl447@nyu.edu>
2021-03-22 11:49:02 -0400

    More writing

--- Laplace/Assets/Scripts/VN/PostAzazelScene.cs
+++ Laplace/Assets/Scripts/VN/PostAzazelScene.cs
@@ -21,9 +21,11 @@ public class PostAzazelScene : MonoBehaviour
 
     Scene current = new Scene();
     int count = 0;
-    public Sprite bg, furfur, furfurH, furfurEww, furfurDis, furfurWow, azazel, azazelF, abyzou, abyzouTsu;
+    public Sprite bg, bgBlank, bgFrontDoor, bgHallway1, bgHallway2, bgHallway3,
+        furfur, furfurH, furfurEww, furfurDis, furfurWow, azazel, azazelF, abyzou, abyzouTsu, abyzouScared,
+        counter, oldFashion, carrotJuice1, carrotJuice2, scaryDoor, modayaalZoom;
 
-    public AudioClip spitTake;
+    public AudioClip spitTake, pour;
 
     void Start()
     {
@@ -116,18 +118,22 @@ public class PostAzazelScene : MonoBehaviour
             "so:Modayaal",
         },
         bg, nine);
+        eight.mini = counter;
 
         nine.Set(new string[]
         {
             "there we go:Modayaal"
         },
         bg, ten);
+        nine.sound = pour;
+        nine.mini = oldFashion;
 
         ten.Set(new string[]
         {
             "and for Furfur:Modayaal"
         },
         bg, eleven);
+        ten.mini = carrotJuice1;
 
         eleven.Set(new string[]
         {
@@ -137,6 +143,7 @@ public class PostAzazelScene : MonoBehaviour
             "Well it's his fault anyways"
         },
         bg, twelve);
+        eleven.mini = carrotJuice2;
 
         twelve.Set(new string[]
         {
@@ -176,11 +183,96 @@ public class PostAzazelScene : MonoBehaviour
             "I'm not blaming you",
             "I know you didn't know this would happen",
             "yeah but it's still my fault:Modayaal",
-            "That's a healthy outlook"
+            "That's a healthy outlook :Azazel",
+            "Well if there's anything I can get you:Modayaal",
+            "I'm here to help"
         },
-        bg);
+        bg, fifteen);
         fourteen.left = azazelF;
         fourteen.right = furfurDis;
+
+        fifteen.Set(new string[]
+        {
+            "Well actually...:Azazel",
+            "I've always been curious",
+            "how do you get around that whole irreversability of entropy thing",
+            "Well you see:Modayaal"
+        },
+        bg, sixteen);
+        fifteen.center = azazel;
+
+        sixteen.Set(new string[]
+        {
+            "Hey y'all Tess here:Tess",
+            "I've decided to redact Modayaal's answer here, ",
+            "as to not spoil the answer for all the physicist aspiring to solve entropy here.",
+            "Thank you for your understanding!"
+        },
+        bgBlank, seventeen);
+
+        seventeen.Set(new string[]
+        {
+            "Wow absolutely fascinating:Azazel",
+            "It's wild how long it took humanity to figure out such a simple concept:Modayaal"
+        },
+        bg, eighteen);
+
+        eighteen.Set(new string[]
+        {
+            "Modey, do you think you should check up on Abyzou?:Furfur",
+            "Yeah I'm getting kind of worried:Azazel",
+            "How hard could it be to find a closet?",
+            "umm I do sleep in the foyer for a reason:Modayaal",
+            "but she really didn't want me to help her",
+            "I think it's time to make the call:Furfur",
+            "well:Modayaal",
+            "*GULP*:Modayaal::I",
+            "wish me luck:Modayaal::N",
+            "Which way do you think they mean?:Azazel",
+            "honestly not sure:Furfur"
+        },
+        bg, nineteen);
+        eighteen.left = azazelF;
+        eighteen.right = furfur;
+
+        nineteen.Set(new string[]
+        {
+            "Okay let's see:Modayaal"
+        },
+        bgFrontDoor, twenty);
+
+        twenty.Set(new string[]
+        {
+            "Hopefully she didn't go this way:Modayaal"
+        },
+        bgFrontDoor, twentyOne);
+        twenty.mini = scaryDoor;
+
+        twentyOne.Set(new string[]
+        {
+            "*CREAK*:The House::I"
+        },
+        bgHallway1, twentyTwo);
+
+        twentyTwo.Set(new string[]
+        {
+            "*URKK*:The House::I"
+        },
+        bgHallway2, twentyThree);
+
+        twentyThree.Set(new string[]
+        {
+            "oh no....:Modayaal::N"
+        },
+        bgHallway3, twentyFour);
+        twentyThree.mini = modayaalZoom;
+
+        twentyFour.Set(new string[]
+        {
+            "what are you doinng here..?."
+        },
+        bgHallway3);
+        twentyFour.center = abyzouScared;
 
         textC = TextControl.instance;
         current = one;
