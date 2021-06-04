d90884025b75c3efd5cb9850d554e71c31e1c641
Tess <srl447@nyu.edu>
2021-06-04 17:50:49 -0400

    Bug Fixes

---
+++ Laplace/Assets/Scripts/Util/VolumeFirstBoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeFirstBoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.GetString("Volume Fix") != "Fixed")
+        {
+            PlayerPrefs.SetFloat("Music Volume", 1);
+            PlayerPrefs.SetFloat("Sound Volume", 1);
+            PlayerPrefs.SetString("Volume Fix", "Fixed");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
